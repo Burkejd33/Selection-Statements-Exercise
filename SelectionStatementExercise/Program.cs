9baf28ec57046774337df27d141a5dd600ffef56
Douglas Burke <douglas.burke@me.com>
2026-05-13 14:05:15 -0500

    Completed Excercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favnumber = 3;
+            Console.WriteLine("Guess my favorite number:");
+            int userInput = int.Parse(Console.ReadLine());
+            if (userInput < favnumber)
+            {
+                Console.WriteLine("too low");
+            }
+            else if (userInput > favnumber)
+            {
+                Console.WriteLine("too high.");
+            }
+            else 
+            {
+                Console.WriteLine("You Guessed it");
+            }
+           Console.WriteLine("Press any key to continue . . .");
+           Console.ReadKey();
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+            switch (favoriteSubject)
+            {
+            case "Math":
+                Console.WriteLine("So you like problem solving");
+                break;
+            case "Science":
+                Console.WriteLine("So you like asking questions");
+                break;
+            case "History":
+                Console.WriteLine("So you like knowing about our past");
+                break;
+            case "English":
+                Console.WriteLine("So you like to follow the rules");
+                break;
+            case "Art":
+                Console.WriteLine("So you are creatvie");
+                break;
+            case "PE":
+                Console.WriteLine("So you like sports");
+                break;
+            default:
+                Console.WriteLine("Interesting choice! I never would have guessed that!");
+                break;
+            }
         }
     }
 }
